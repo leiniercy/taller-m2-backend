3196a3ae835bbddd7b55b63ea1bfda6c4911f442
Leinier Caraballo Yanes <94509566+leiniercy@users.noreply.github.com>
2023-09-01 21:48:09 -0400

    configure los Cors

--- src/main/java/tallerM2/tallerM2/configuration/CorsConfiguration.java
+++ src/main/java/tallerM2/tallerM2/configuration/CorsConfiguration.java
@@ -5,6 +5,7 @@
  */
 package tallerM2.tallerM2.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,19 +15,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  *
  * @author Admin
  */
-//@Configuration
+@Configuration
 public class CorsConfiguration {
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//               registry.addMapping("/**").allowedMethods("GET","PUT","POST","DELETE").allowedOrigins("http://localhost:3000");
-//            }
-//              
-//        };
-//
-//    }
+    @Value("${taller2M.app.allowed.origin.local}")
+    private String url_local;
+    @Value("${taller2M.app.allowed.origin.ext}")
+    private String url_ext;
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+               registry.addMapping("/**")
+                       .allowedMethods("GET","PUT","POST","DELETE") //tipos de peticiones aceptadas
+                       .allowedOrigins(url_local,url_ext); // url de donde se reciben las peticiones
+            }
+
+        };
+
+    }
 
 }
